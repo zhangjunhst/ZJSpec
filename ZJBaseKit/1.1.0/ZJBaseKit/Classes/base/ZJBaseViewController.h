10a6eb764f486fcbdf56597f73c70b421bba0723
ZJ <ZJ>
2021-03-15 11:59:15 +0800

    ces

---
+++ ZJBaseKit/1.1.0/ZJBaseKit/Classes/base/ZJBaseViewController.h
@@ -0,0 +1,17 @@
+//
+//  ZJBaseViewController.h
+//  ZJBaseKit
+//
+//  Created by 张军 on 2021/3/12.
+//  Copyright © 2021 张军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZJBaseViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ZJBaseKit/1.1.0/ZJBaseKit/Classes/base/ZJBaseViewController.m
@@ -0,0 +1,34 @@
+//
+//  ZJBaseViewController.m
+//  ZJBaseKit
+//
+//  Created by 张军 on 2021/3/12.
+//  Copyright © 2021 张军. All rights reserved.
+//
+
+#import "ZJBaseViewController.h"
+
+@interface ZJBaseViewController ()
+
+@end
+
+@implementation ZJBaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor orangeColor];
+    self.hidesBottomBarWhenPushed = YES;
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
